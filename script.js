dc79333abce5946b905df71d9ca81068cc015e01
MimiPark06 <dumawal1820241@mkt.ceu.edu.ph>
2019-01-31 16:13:17 +0800

    Background/Title

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+    windowTop = $(window).scrollTop();
+
+    $('.js-cloud-1').css({
+      'transform':'translateX('+(windowTop) * .3 +'px)'
+    });
+
+    $('.js-cloud-2').css({
+      'transform':'translateX('+(windowTop) * .7 +'px)'
+    });
+  });
+});
